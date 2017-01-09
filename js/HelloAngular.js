89c429c33d715e4ae99228c1576db28c9b15229a
xyoscer <1498546245@qq.com>
2017-01-09 18:39:51 +0800

    add form controller

--- js/HelloAngular.js
+++ js/HelloAngular.js
@@ -12,9 +12,40 @@
         text:'Hello'
     };
 }*/
-var helloModule = angular.module('HelloAngular',[]);
-helloModule.controller('helloNgCtrl',['$scope',function($scope){
-    $scope.greeting = {
-        text: 'hello'
-    };
-}]);
+ angular.module('myApp',[])
+       .controller('SignUpController',function($scope){
+         $scope.userdata = {};
+         $scope.submitForm = function() {
+               console.log($scope.userdata);
+              if($scope.signUpForm.$invalid){
+                alert("请填写完信息");
+              }else {
+                 alert("注册成功");
+             }
+          };
+       })
+       .directive('compare',function(){
+           return {
+               require:'ngModel',
+               restrict:'A',//匹配模式 A属性
+               scope:{orgTxt: '=compare'},
+               link: function(sco,ele,att,con) {
+                   con.validators.compare = function(v) {
+                       return  v === sco.orgTxt;
+                   };
+               }
+           };
+          /*var obj = {};
+           obj.restrict = 'AE';
+           obj.scope = {orgText: '=compare'};
+           obj.require = 'ngModel';
+           obj.link = function(sco,ele,att,ngModel) {
+               ngModel.$validators.compare = function(v) {
+                   return v == sco.orgText;
+               };
+               sco.$watch('orgText',function(){
+                   ngModel.$validate();
+                   });
+           };
+           return obj;*/
+       });
